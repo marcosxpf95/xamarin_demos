6f5a22c25f7a05e25489f5daf2df77f2810d5780
Marcos Xavier <marcos.desenvolvimento95@gmail.com>
2019-12-15 14:14:26 -0300

    1.0

---
+++ DemoXamarin/Models/Usuario.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace DemoXamarin.Models
+{
+    public class Usuario
+    {
+        public string nomeUsuario { get; set; }
+        public string respostaSecreta { get; set; }
+
+        public Usuario() {}
+
+        public Usuario(string nomeUsuario, string respostaSecreta)
+        {
+            this.nomeUsuario = nomeUsuario;
+            this.respostaSecreta = respostaSecreta;
+        }
+
+        public bool ValidarLogin(string respostaSecreta)
+        {
+            if (respostaSecreta == null)
+                return false;
+
+            
+            if(!this.nomeUsuario.Equals("") && !this.respostaSecreta.Equals(""))
+            {
+                if (respostaSecreta.Equals("palmeiras"))
+                {
+                    return true;
+                }
+                return false;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ DemoXamarin/Views/Login.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using DemoXamarin.Models;
+using Xamarin.Forms;
+
+namespace DemoXamarin.Views
+{
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        void Logar(object sender, EventArgs e)
+        {
+            Usuario user = new Usuario(Entry_Usuario.Text, Entry_RespostaSecreta.Text);
+
+            if(user.ValidarLogin(user.respostaSecreta))
+            {
+                DisplayAlert("Parabéns","Parabéns, você entende sobre futebol. Resposta secreta está correta!","Ok");
+            }
+            else
+            {
+                DisplayAlert("Falha", "Você não entende sobre futebol ou todos os dados não foram preenchidos.", "Ok");
+            }
+        
+        }
+    }
+}
